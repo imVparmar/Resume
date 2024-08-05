905b060dc8ae91e17bd0af698751e2dc879a3f80
imVparmar <vinitparmarsocial@gmail.com>
2024-08-05 22:05:59 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // Smooth scrolling
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 70
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
